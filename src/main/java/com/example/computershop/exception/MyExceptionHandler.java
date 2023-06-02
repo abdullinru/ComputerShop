c46bc8ca947604a9be5883fdc10a19685e39636d
abdullinru <abdullin17021989@gmail.com>
2023-06-03 00:58:07 +0600

    создал папку с Эксепшнами, написал обработчик ошибок

---
+++ src/main/java/com/example/computershop/exception/MyExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.example.computershop.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class MyExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handlerException (IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(String.format("Error: %s", e.getMessage()));
+    }
+    @ExceptionHandler(ComputerNotFoundException.class)
+    public ResponseEntity<String> handlerException (ComputerNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(String.format("Error: %s", e.getMessage()));
+    }
+}
