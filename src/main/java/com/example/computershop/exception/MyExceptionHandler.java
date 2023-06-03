f6e8108294a835a900ceeb3d2e4ef63bc01a5516
abdullinru <abdullin17021989@gmail.com>
2023-06-03 13:26:32 +0600

    создал общее исключение ProductNotFoundException, добавил в обработчик исключение

--- src/main/java/com/example/computershop/exception/MyExceptionHandler.java
+++ src/main/java/com/example/computershop/exception/MyExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.computershop.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -15,5 +16,9 @@ public class MyExceptionHandler {
     @ExceptionHandler(ComputerNotFoundException.class)
     public ResponseEntity<String> handlerException (ComputerNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(String.format("Error: %s", e.getMessage()));
+    }
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handlerException (HttpMessageNotReadableException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(String.format("Error: %s", e.getMessage()));
     }
 }
